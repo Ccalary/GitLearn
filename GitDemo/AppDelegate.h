dbe9ac4eb59a1926f9d7fbe5515302c292247270
caohouhong <229336009@qq.com>
2018-11-06 23:12:31 +0800

    提交

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by caohouhong on 2018/11/6.
+//  Copyright © 2018年 caohouhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by caohouhong on 2018/11/6.
+//  Copyright © 2018年 caohouhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
