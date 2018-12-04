21dd329ed3e8c0245ac7deaa1fbef4b2f8fc6801
caohouhong <229336009@qq.com>
2018-12-04 20:12:31 +0800

    增加方法

--- GitDemo/ViewController.m
+++ GitDemo/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 
@@ -25,5 +25,8 @@
     // Dispose of any resources that can be recreated.
 }
 
-
+// 主应用方法
+- (void)masterFun {
+    
+}
 @end
